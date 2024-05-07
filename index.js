67fc5d60d1a5f52b53b1f7e103eeb78d6d928105
HAMZA <165916716+HAMZAKAFEEL@users.noreply.github.com>
2024-05-07 06:59:11 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "AJJ SODA KIA AAYE GA...",
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "OUR KIA AAYE GA...",
+            default: "false"
+        }
+    ]);
+    todos.push(addTask.todo);
+    condition = addTask.addMore;
+    console.log(todos);
+}
